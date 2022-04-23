6341d0589dd68b3e44377be4dc645b3aba708eeb
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-04-23 13:17:55 -0400

    Added bulma and jquery

--- tra/__init__.py
+++ tra/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
 
 app = Flask(__name__)
+app.secret_key = '9174d8959d3b00b2ca44f6e0114621f3b1fe138c1de17da6509a76838171653a'
 # this just has to be here for magic reasons
 from tra import routes
+
 
 def main():
     app.run("0.0.0.0", 8000, debug=True)
--- tra/routes.py
+++ tra/routes.py
@@ -1,5 +1,7 @@
+from flask import render_template
 from tra import app
+
 
 @app.route("/")
 def home():
-    return "<h1>Hello, World</h1>"
+    return render_template("base.html") 
