8375accc2439edadecebe7c43e9469e59b4a7588
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-04-23 16:55:43 -0400

    Changed the structure to be package compatible

--- tra/__init__.py
+++ tra/__init__.py
@@ -1,4 +1,15 @@
-from tra.server import *
+from flask import Flask
+from datetime import timedelta
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__) #instance of flask
+app.secret_key = b'\x14J\xff\x00\x9c\xf3\x80\xab\xda\r8\xa9\xad3D\xab'  #encryption key for session data
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3" #config for database. data.sqlite3 is the table name
+app.config["SQLALCEHMY_TRACK_MODIFICATIONS"] = False
+app.permanent_session_lifetime = timedelta(minutes=5)
+db = SQLAlchemy(app) #create a database object
+# needed for magic reasons
+from tra import routes
 
 def main():
 	db.create_all()
