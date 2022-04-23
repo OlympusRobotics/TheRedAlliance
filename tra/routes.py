54c4b4c3bc02b97f6307daf2c2982572a8fbe18b
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-04-23 19:26:34 -0400

    login page done

--- tra/routes.py
+++ tra/routes.py
@@ -1,15 +1,30 @@
 from tra import db, app
 from tra.models import UserData
-from flask import request, session, render_template, flash
+from flask import redirect, request, session, render_template, flash, escape, url_for
 
 @app.route("/")
 @app.route("/home")
 def home():
+    if "name" not in session:
+        return redirect(url_for("login"))
+    # if the session name exists but has nothing in it, redirect to login page
+    if len(session["name"].split("-")) != 2:
+        return redirect(url_for("login"))
     return render_template("home.html")
+
+@app.route("/login")
+def login():
+    if "firstname" in request.form and "lastname" in request.form: 
+        # Add the user's name to the session 
+        session["name"] = escape(f"{request.form['firstname']}-{request.form['lastname']}") 
+        print(session)
+        return redirect(url_for("home"))
+        
+    return render_template("login.html")
 
 # This route is for testing writing to sql database
 @app.route("/test", methods=["POST", "GET"])  # specify methods and route
-def login():
+def test():
 	if request.method == "POST":
 		session.permanent = True # set permanent session
 		form_data = request.form["scout_data"]
