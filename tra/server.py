0dba326894e7efeee82a2a80da1da4b18f571828
Jason Wang <jasonxwang9@gmail.com>
2022-04-23 13:37:46 -0400

    fixed more formmating

--- tra/server.py
+++ tra/server.py
@@ -2,7 +2,6 @@ from flask import Flask, redirect, url_for, render_template, request, session, f
 from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
-
 app = Flask(__name__) #instance of flask
 app.secret_key = b'\x14J\xff\x00\x9c\xf3\x80\xab\xda\r8\xa9\xad3D\xab'  #encryption key for session data
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.sqlite3" #config for database. data.sqlite3 is the table name
@@ -17,3 +16,19 @@ class UserData(db.Model):#create table
 
 	def __init__(self, data):
 		self.data = data
+@app.route("/", methods=["POST", "GET"])  # specify methods and route
+def login():
+	if request.method == "POST":
+		session.permanent = True # set permanent session
+		form_data = request.form["scout_data"]
+
+		#found_user = users.query.filter_by(name=user).first() #what_to_access.perform_a_query.find_all_that_meet_criteria.grab_first_result    This is how to find a user
+		usr_data = UserData(form_data)
+		db.session.add(usr_data)
+		db.session.commit()
+		for i in UserData.query.all():
+			print(i.data)
+
+		flash("Data recieved!", "info")
+	
+	return render_template("scouting.html")
