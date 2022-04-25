3c57ef1d654157cd0202716a0b89a5d6fa02c142
Abdullah Mohammed <aamohammed4556@gmail.com>
2022-04-25 18:33:50 +0000

    login now works

--- tra/models.py
+++ tra/models.py
@@ -1,8 +1,20 @@
 from tra import db
+	
+class Scout(db.Model):
+	id = db.Column("id", db.Integer, primary_key=True) # pk
+	name = db.Column(db.String(64))
+	code = db.Column(db.String(12)) # code used for accessing account
+	responses = db.relationship('ScoutResponse', backref='scout', lazy=True)
+	def __init__(self, name, code):
+		self.name = name
+		self.code = code
+	
+	def __repr__(self):
+		return f"< {self.name}, {self.code}, {self.id} >"
 
-class UserData(db.Model):#create table
+class ScoutResponse(db.Model):#create table
 	_id = db.Column("id", db.Integer, primary_key=True) # pk
-	data = db.Column(db.String(1000000)) # json of scouting info per user
-
+	scout_id = db.Column(db.Integer, db.ForeignKey('scout.id'))
+	data = db.Column(db.String(1000000)) # json of the response provided by the scout
 	def __init__(self, data):
 		self.data = data
--- tra/routes.py
+++ tra/routes.py
@@ -1,5 +1,5 @@
 from tra import db, app
-from tra.models import UserData
+from tra.models import ScoutResponse, Scout
 from flask import redirect, request, session, render_template, flash, escape, url_for
 
 @app.route("/")
@@ -8,17 +8,27 @@ def home():
     if "name" not in session:
         return redirect(url_for("login"))
     # if the session name exists but has nothing in it, redirect to login page
-    if len(session["name"].split("-")) != 2:
+    scout = Scout.query.filter_by(name=session["name"]).first()
+    if scout is None:
         return redirect(url_for("login"))
-    return render_template("home.html")
+    return render_template("home.html", scout=scout)
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    if "firstname" in request.form and "lastname" in request.form: 
-        # Add the user's name to the session 
-        session["name"] = escape(f"{request.form['firstname']}-{request.form['lastname']}") 
-        print(session)
-        return redirect(url_for("home"))
+    if request.method == 'POST':
+        if "name" in request.form and "code" in request.form: 
+            # Add the user's name to the session 
+            scout = Scout.query.filter_by(name=request.form["name"]).first()
+            # make sure a user exists with that name
+            if scout is not None:
+                # check code
+                if request.form["code"] == scout.code: 
+                    # add username to session 
+                    session["name"] = escape(request.form["name"]) 
+                    return redirect(url_for("home"))
+
+            flash("Invalid Scout Credentials")
+                
         
     return render_template("login.html")
 
@@ -30,7 +40,7 @@ def test():
 		form_data = request.form["scout_data"]
 
 		#found_user = users.query.filter_by(name=user).first() #what_to_access.perform_a_query.find_all_that_meet_criteria.grab_first_result    This is how to find a user
-		usr_data = UserData(form_data)
+		usr_data = ScoutResponse(form_data)
 		db.session.add(usr_data)
 		db.session.commit()
 		for i in UserData.query.all():
